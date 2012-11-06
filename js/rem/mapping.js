80b84405fb3b2dc1ae8f0c424658e5f22fecdb37
Xinyu <claliu@gmail.com>
2012-11-06 11:17:02 +0800

    prod ggl ONLY

--- js/rem/mapping.js
+++ js/rem/mapping.js
@@ -1,16 +1,18 @@
-/* REM Map Version 1.01 */
+/* REM Map Version v1.0.2 Google map ONLY */
 var zoom = 13;
 var maxZoom = 18;
 var ggl = new L.Google();
+/*
 var bng = new L.BingLayer(
 	"AjLuUJvNNUSZBDz_xYPYy51rXL6hZMqaPbJQvwIf-CYHeL_sTxlGpZ-_bf67S9cv");
+*/
 var datarent = new L.geoJson(datarent, {
 	onEachFeature: onEachFeature
 });
 var datasale = new L.geoJson(datasale, {
 	onEachFeature: onEachFeature
 });
-var baseMaps = {"交通地图": ggl, "航拍地图": bng};
+var baseMaps = {"交通地图": ggl};
 var data = {"在售地产": datasale, "地产租赁": datarent};
 var map = new L.map('map', {
 	center: center, 
@@ -28,4 +30,4 @@ function onEachFeature(feature, layer) {
 		feature.properties.detail + ' ' +
 		feature.properties.comment);
 }
-/* REM Map Version 1.01 */
+/* REM Map Version v1.0.2 Google map ONLY */
