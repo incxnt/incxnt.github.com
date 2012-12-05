f8ba8998fc348ca9ece11d3bda52b20ca9530eb3
Xinyu <claliu@gmail.com>
2012-12-05 18:55:45 +0800

    2324 new feature

--- js/s/2324.js
+++ js/s/2324.js
@@ -29,3 +29,18 @@ function style(feature) {
         fillOpacity: 0.7
     };
 }
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+}
