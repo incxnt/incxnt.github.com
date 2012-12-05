341d38950809d58b7c8136ac10771046f459cace
Xinyu <claliu@gmail.com>
2012-12-05 18:52:59 +0800

    2324 style state

--- js/s/2324.js
+++ js/s/2324.js
@@ -6,4 +6,26 @@ var cloudmade = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{
 	styleId: 22677
 }).addTo(map);
 
-L.geoJson(statesData).addTo(map);
+L.geoJson(statesData, {style: style}).addTo(map);
+
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
