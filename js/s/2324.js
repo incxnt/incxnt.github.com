079815156a61b44eee7fa2f72abcff129124b562
Xinyu <claliu@gmail.com>
2012-12-05 19:16:38 +0800

    2324 legend

--- js/s/2324.js
+++ js/s/2324.js
@@ -12,6 +12,25 @@ geojson = L.geoJson(statesData, {
 	onEachFeature: onEachFeature
 }).addTo(map);
 
+
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+        : 'Hover over a state');
+};
+
+info.addTo(map);
+
+
 function getColor(d) {
     return d > 1000 ? '#800026' :
            d > 500  ? '#BD0026' :
