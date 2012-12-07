06df6ab65cc4f5ec65e6c359fd6c1fca965bae64
Xinyu <claliu@gmail.com>
2012-12-07 15:32:29 +0800

    mapping misc

--- js/s/2324.js
+++ js/s/2324.js
@@ -33,28 +33,21 @@ info.update = function (props) {
         '<b>' + props.name + '</b><br />' + props.density + ' people / km<sup>2</sup>'
         : 'Hover over a province');
 };
-
 info.addTo(map);
 
 
 var legend = L.control({position: 'bottomright'});
-
 legend.onAdd = function (map) {
-
     var div = L.DomUtil.create('div', 'info legend'),
         grades = [0, 10, 20, 50, 100, 200, 500, 1000],
         labels = [];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < grades.length; i++) {
         div.innerHTML +=
             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
-
     return div;
 };
-
 legend.addTo(map);
 
 
