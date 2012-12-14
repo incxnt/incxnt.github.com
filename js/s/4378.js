b86e9dbd3064719123f315b1ebe5ba8d0422138f
Xinyu <claliu@gmail.com>
2012-12-14 15:23:39 +0800

    airports

---
+++ js/s/4378.js
@@ -0,0 +1,33 @@
+var cloudmadeKey = 'e6e22dcc8e474b679084e83b0a0845fc';
+var cloudmadeStyle = 22677;
+var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/' + cloudmadeKey + '/' + cloudmadeStyle + '/256/{z}/{x}/{y}.png';
+var cloudmadeMap = new L.tileLayer(cloudmadeUrl);
+
+var mapCenter = new L.LatLng(35.86166, 104.195397);
+var initZoom = 2;
+var minZoom = 1;
+var maxZoom = 6;
+var map = new L.map('map', {
+    attributionControl: false,
+    center: mapCenter,
+    zoom: initZoom,
+    minZoom: minZoom,
+    maxZoom: maxZoom,
+    layers: [cloudmadeMap]
+});
+
+var geojson = L.geoJson(airports).addTo(map);
+
+
+var info = L.control();
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Population Density</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' + props.density + ' people / km<sup>2</sup>'
+        : 'Hover over a province');
+};
+info.addTo(map);
