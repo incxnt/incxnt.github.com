a9217bb7b00d76be8666f3b6f76297fe99486663
Xinyu <claliu@gmail.com>
2012-12-24 00:03:34 +0800

    beijing

---
+++ js/xnt/9369.js
@@ -0,0 +1,44 @@
+var cloudmadeKey = 'e6e22dcc8e474b679084e83b0a0845fc';
+var cloudmadeStyle = 22677;
+var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/' + cloudmadeKey + '/' 
+                    + cloudmadeStyle + '/256/{z}/{x}/{y}.png';
+var cloudmadeMap = new L.tileLayer(cloudmadeUrl);
+
+var mapCenter = new L.LatLng(39.90403, 116.407526);
+var initZoom = 2;
+var minZoom = 2;
+var maxZoom = 7;
+var map = new L.map('map', {
+    attributionControl: false,
+    center: mapCenter,
+    zoom: initZoom,
+    minZoom: minZoom,
+    maxZoom: maxZoom,
+    layers: [cloudmadeMap]
+});
+
+var info = L.control();
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'map-info');
+    this.update();
+    return this._div;
+};
+info.update = function () {
+    this._div.innerHTML = '<h4>Beijing</h4>';
+};
+info.addTo(map);
+
+
+
+var beijing = L.geoJson(beijing, {
+    onEachFeature: onEachFeature
+});
+beijing.addTo(map);
+
+
+
+
+
+function onEachFeature(feature, layer) {
+    layer.bindPopup(feature.properties.NAME_3);
+}
