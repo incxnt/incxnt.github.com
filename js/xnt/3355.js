c45cca511b84d3c92b5941bdb86d312939743850
Xinyu <claliu@gmail.com>
2012-12-26 00:06:30 +0800

    0

---
+++ js/xnt/3355.js
@@ -0,0 +1,110 @@
+var cloudmadeKey = 'e6e22dcc8e474b679084e83b0a0845fc';
+var cloudmadeStyle = 22677;
+var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/' + cloudmadeKey + '/' + cloudmadeStyle + '/256/{z}/{x}/{y}.png';
+var cloudmadeMap = new L.tileLayer(cloudmadeUrl);
+
+var mapCenter = new L.LatLng(35.86166, 104.195397);
+var initZoom = 4;
+var minZoom = 4;
+var maxZoom = 4;
+var map = new L.map('map', {
+    attributionControl: false,
+    center: mapCenter,
+    zoom: initZoom,
+    minZoom: minZoom,
+    maxZoom: maxZoom,
+    layers: [cloudmadeMap]
+});
+
+var geojson = L.geoJson(provinceData, {
+	style: style,
+	onEachFeature: onEachFeature
+}).addTo(map);
+
+
+var info = L.control();
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Population Density</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' + props.density + ' people / km<sup>2</sup>'
+        : 'Hover over a province');
+};
+info.addTo(map);
+
+
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [];
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
+legend.addTo(map);
+
+
+
+
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
