4879853e0f1ff5e8a29c728575553f9db9ed8b13
Xinyu <claliu@gmail.com>
2012-12-26 00:19:15 +0800

    0

--- js/xnt/3355.js
+++ js/xnt/3355.js
@@ -3,7 +3,7 @@ var cloudmadeStyle = 22677;
 var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/' + cloudmadeKey + '/' + cloudmadeStyle + '/256/{z}/{x}/{y}.png';
 var cloudmadeMap = new L.tileLayer(cloudmadeUrl);
 
-var mapCenter = new L.LatLng(35.86166, 104.195397);
+var mapCenter = new L.LatLng(40.25, 116.407526);
 var initZoom = 4;
 var minZoom = 4;
 var maxZoom = 4;
