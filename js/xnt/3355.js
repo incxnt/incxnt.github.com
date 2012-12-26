3bd0bbea1f6e097dd1ac8ca88b219183746f0cd0
Xinyu <claliu@gmail.com>
2012-12-26 15:46:20 +0800

    pek

--- js/xnt/3355.js
+++ js/xnt/3355.js
@@ -28,7 +28,7 @@ info.onAdd = function (map) {
     return this._div;
 };
 info.update = function (props) {
-    this._div.innerHTML = '<h4>Beijing</h4>' +  (props ?
+    this._div.innerHTML = '<h4>北京市</h4>' +  (props ?
         '<b>' + props.name + '</b><br />'
         : 'Hover over the map');
 };
@@ -55,19 +55,19 @@ legend.addTo(map);
 
 
 
-function getColor(name) {
-    switch (name) {
-            case 'Beijing': Color = '#A6CEE3'; break;
-            case 'Changping': Color = '#1F78B4'; break;
-            case 'Daxing': Color = '#B2DF8A'; break;
-            case 'Fangshan': Color = '#33A02C'; break;
-            case 'Huairou': Color = '#FB9A99'; break;
-            case 'Mentougou': Color = '#E31A1C'; break;
-            case 'Miyun': Color = '#FDBF6F'; break;
-            case 'Pinggu': Color = '#FF7F00'; break;
-            case 'Shunyi': Color = '#CAB2D6'; break;
-            case 'Tongzhou': Color = '#6A3D9A'; break;
-            case 'Yanqing': Color = '#FFFF99'; break;      
+function getColor(id) {
+    switch (id) {
+            case 78: Color = '#A6CEE3'; break;
+            case 79: Color = '#1F78B4'; break;
+            case 80: Color = '#B2DF8A'; break;
+            case 81: Color = '#33A02C'; break;
+            case 82: Color = '#FB9A99'; break;
+            case 83: Color = '#E31A1C'; break;
+            case 84: Color = '#FDBF6F'; break;
+            case 85: Color = '#FF7F00'; break;
+            case 86: Color = '#CAB2D6'; break;
+            case 87: Color = '#6A3D9A'; break;
+            case 88: Color = '#FFFF99'; break;      
     }
     return Color;
 }
@@ -75,7 +75,7 @@ function getColor(name) {
 
 function style(feature) {
     return {
-        fillColor: getColor(feature.properties.name),
+        fillColor: getColor(feature.properties.id),
         fillOpacity: 0.7,
         color: '#F0FFFF',
         opacity: 1,
