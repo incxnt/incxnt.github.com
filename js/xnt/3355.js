53a3efd581c93d87311ddddb54abb138f80497f9
Xinyu <claliu@gmail.com>
2012-12-26 15:08:20 +0800

    pek

--- js/xnt/3355.js
+++ js/xnt/3355.js
@@ -91,10 +91,10 @@ function highlightFeature(e) {
     var layer = e.target;
 
     layer.setStyle({
-        weight: 5,
+        weight: 3,
         color: '#666',
         dashArray: '',
-        fillOpacity: 0.7
+        fillOpacity: 0.9
     });
 
     if (!L.Browser.ie && !L.Browser.opera) {
