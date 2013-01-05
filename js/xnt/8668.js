57c462fc562ace88c73b60407fee3880dfef11c8
Xinyu <claliu@gmail.com>
2013-01-06 01:19:07 +0800

    weather

--- js/xnt/8668.js
+++ js/xnt/8668.js
@@ -7,6 +7,10 @@ var mapquestUrl = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png';   //ma
 var subDomains = ['otile1','otile2','otile3','otile4'];
 var mapquestMap = new L.TileLayer(mapquestUrl, {subdomains: subDomains});
 
+var snow = L.tileLayer('http://{s}.tile.openweathermap.org/map/snow/{z}/{x}/{y}.png');
+var rain = L.tileLayer('http://{s}.tile.openweathermap.org/map/rain/{z}/{x}/{y}.png');
+var precipitation = L.tileLayer('http://{s}.tile.openweathermap.org/map/precipitation/{z}/{x}/{y}.png');
+var clouds = L.tileLayer('http://{s}.tile.openweathermap.org/map/clouds/{z}/{x}/{y}.png');
 
 var mapCenter = new L.LatLng(35.86166, 104.195397);
 var initZoom = 4;
@@ -18,8 +22,17 @@ var map = new L.map('map', {
     zoom: initZoom,
     minZoom: minZoom,
     maxZoom: maxZoom,
-    layers: [mapquestMap]
+    layers: [mapquestMap, clouds]
 });
+
+var overlayMaps = {
+    "Cloud Cover": clouds,
+    "Rain precipitation": rain,
+    "Snow precipitation": snow,
+    "Quantity of precipitation": precipitation
+}
+
+L.control.layers(null, overlayMaps).addTo(map);
 
 var info = L.control();
 info.onAdd = function (map) {
@@ -32,4 +45,4 @@ info.update = function (props) {
 };
 info.addTo(map);
 
-L.tileLayer('http://{s}.tile.openweathermap.org/map/snow/{z}/{x}/{y}.png').addTo(map); // clouds snow rain precipitation
+
