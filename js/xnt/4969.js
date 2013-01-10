1ebad2d0699c057240a06339b3b98bbdbe418be4
Xinyu <claliu@gmail.com>
2013-01-11 00:00:25 +0800

    d

--- js/xnt/4969.js
+++ js/xnt/4969.js
@@ -1,10 +1,14 @@
-var data = [1, 2, 3];
+var data = [40, 20, 10];
 
-var p = d3.select("#xnt-viz")
-            .selectAll("p")
-            .data(data)
-            .enter()
-            .append("p")
-            .text(function (d, i) {
-                return "i = " + i + ", d = " + d;
-            });
+var svg = d3.select("#xnt-viz").append("svg")
+                                .attr("width", 600)
+                                .attr("height", 100);
+
+var circle = svg.selectAll("circle")
+                .data(data)
+                .enter()
+                .append("circle")
+                .attr("cx", 50)
+                .attr("cy", 50)
+                .attr("r", function (d) {return d;})
+                .style("fill", "green");
