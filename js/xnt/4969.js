6cc08c2f3633620d30ff59bfaff2f34649fe3256
Xinyu <claliu@gmail.com>
2013-01-11 00:33:13 +0800

    d

--- js/xnt/4969.js
+++ js/xnt/4969.js
@@ -1,22 +1,4 @@
-var data = [40, 20, 10];
-
 var svg = d3.select("#xnt-viz").append("svg")
-                                .attr("width", 600)
-                                .attr("height", 100);
-
-var circle = svg.selectAll("circle")
-                .data(data)
-                .enter()
-                .append("circle")
-                .attr("cx", 50)
-                .attr("cy", 50)
-                .attr("r", function (d) {return d;})
-                .style("fill", function (d) {
-                    var color;
-                    switch (d) {
-                            case 40: color = "green"; break;
-                            case 20: color = "purple"; break;
-                            case 10: color = "red"; break;
-                    }
-                    return color;
-                });
+                                .attr("width", 200)
+                                .attr("height", 200)
+                                .style("border", "1px solid black");
