6b59c9ac04a419376f59a12765e3cdfb14b177dd
Xinyu <claliu@gmail.com>
2013-01-10 17:32:06 +0800

    d3 circle

--- js/xnt/4969.js
+++ js/xnt/4969.js
@@ -1,1 +1,10 @@
-d3.select("#xnt-viz").append("p");
+d3.select("#xnt-viz")
+    .append("svg")
+    .attr("width", 50)
+    .attr("height", 50)
+    .append("circle")
+    .attr("cx", 25)
+    .attr("cy", 25)
+    .attr("r", 25)
+    .style("fill", "purple")
+    ;
