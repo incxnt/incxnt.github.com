2d4b291ecdbcea25e18daa34b0b3317d6607a793
Xinyu <claliu@gmail.com>
2013-01-11 01:17:01 +0800

    d

--- js/xnt/4969.js
+++ js/xnt/4969.js
@@ -1,4 +1,21 @@
-var data = [30, 70, 110];
+var data = [
+    { 
+        "x_axis": 30,
+        "y_axis": 30,
+        "radius": 20,
+        "color" : "green"
+    }, {
+        "x_axis": 70,
+        "y_axis": 70,
+        "radius": 20,
+        "color" : "purple"
+    }, {
+        "x_axis": 110,
+        "y_axis": 100,
+        "radius": 20,
+        "color" : "red"
+    }
+];
 
 var svg = d3.select("#xnt-viz").append("svg")
                                 .attr("width", 200)
@@ -8,15 +25,7 @@ var circle = svg.selectAll("circle")
                             .data(data)
                             .enter()
                             .append("circle")
-                            .attr("cx", function (d) {return d;})
-                            .attr("cy", function (d) {return d;})
-                            .attr("r", 20)
-                            .style("fill", function (d) {
-                                var color;
-                                switch (d) {
-                                        case 30: color = "green"; break;
-                                        case 70: color = "purple"; break;
-                                        case 110: color = "red"; break;
-                                }
-                                return color;
-                            });
+                            .attr("cx", function (d) {return d.x_axis;})
+                            .attr("cy", function (d) {return d.y_axis;})
+                            .attr("r", function (d) {return d.radius;})
+                            .style("fill", function (d) {return d.color;});
