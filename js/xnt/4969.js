af4fb5b81207f6a319fd1f15ba973c1db74edd67
Xinyu <claliu@gmail.com>
2013-01-11 16:07:24 +0800

    d

--- js/xnt/4969.js
+++ js/xnt/4969.js
@@ -1,31 +1,23 @@
-var data = [
-    { 
-        "x_axis": 30,
-        "y_axis": 30,
-        "radius": 20,
-        "color" : "green"
-    }, {
-        "x_axis": 70,
-        "y_axis": 70,
-        "radius": 20,
-        "color" : "purple"
-    }, {
-        "x_axis": 110,
-        "y_axis": 100,
-        "radius": 20,
-        "color" : "red"
-    }
-];
+var lineD = [
+    {"x": 1, "y": 5},
+    {"x": 20, "y": 20},
+    {"x": 40, "y": 10},
+    {"x": 60, "y": 40},
+    {"x": 80, "y": 5},
+    {"x": 100, "y": 60}
+]
+    
+var lineF = d3.svg.line ()
+                    .x (function (d) {return d.x;})
+                    .y (function (d) {return d.y;})
+                    .interpolate ("linear");
 
 var svg = d3.select("#xnt-viz").append("svg")
                                 .attr("width", 200)
                                 .attr("height", 200);
 
-var circle = svg.selectAll("circle")
-                            .data(data)
-                            .enter()
-                            .append("circle")
-                            .attr("cx", function (d) {return d.x_axis;})
-                            .attr("cy", function (d) {return d.y_axis;})
-                            .attr("r", function (d) {return d.radius;})
-                            .style("fill", function (d) {return d.color;});
+var lineG = svg.append("path")
+                .attr("d", lineF(lineD))
+                .attr("stroke", "blue")
+                .attr("stroke-width", 2)
+                .attr("fill", "none");
