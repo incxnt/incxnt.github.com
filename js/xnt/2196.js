0b012357371993e23c61d285e69864c1c6e437ee
Xinyu <claliu@gmail.com>
2013-01-21 18:10:30 +0800

    uk

--- js/xnt/2196.js
+++ js/xnt/2196.js
@@ -5,7 +5,7 @@ var svg = d3.select("#xntviz").append("svg")
         .attr("width", width)
         .attr("height", height);
 
-d3.json("/data/uk.json", function(error, uk) {
+d3.json("uk.json", function(error, uk) {
     svg.append("path")
         .datum(topojson.object(uk, uk.objects.subunits))
         .attr("d", d3.geo.path().projection(d3.geo.mercator()));
