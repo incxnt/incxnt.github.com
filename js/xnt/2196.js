bb47e6eacb4ff98eb1f4bf31e2f2d1efd0d2a30d
Xinyu <claliu@gmail.com>
2013-01-21 19:59:55 +0800

    uk

--- js/xnt/2196.js
+++ js/xnt/2196.js
@@ -5,8 +5,24 @@ var svg = d3.select("#xntviz").append("svg")
         .attr("width", width)
         .attr("height", height);
 
+var subunits = topojson.object(uk, uk.objects.subunits);
+
+var projection = d3.geo.mercator()
+                .scale(500)
+                .translate([width / 2, height / 2]);
+
+var path = d3.geo.path()
+            .projection(projection);
+
+svg.append("path")
+    .datum(subunits)
+    .attr("d", path);
+
+
+/*
 d3.json("uk.json", function(error, uk) {
     svg.append("path")
         .datum(topojson.object(uk, uk.objects.subunits))
         .attr("d", d3.geo.path().projection(d3.geo.mercator()));
 });
+*/
