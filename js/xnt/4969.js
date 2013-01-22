7f61215b112591ee6228811d8f9a4bea75938da6
Xinyu <claliu@gmail.com>
2013-01-22 19:51:17 +0800

    d3

--- js/xnt/4969.js
+++ js/xnt/4969.js
@@ -1,6 +1,6 @@
 var jsonRectangles = [
     { "x_axis": 10, "y_axis": 10, "height": 20, "width":20, "color" : "green" },
-    { "x_axis": 160, "y_axis": 40, "height": 20, "width":20, "color" : "purple" }
+    { "x_axis": 160, "y_axis": 40, "height": 20, "width":20, "color" : "purple" },
     { "x_axis": 70, "y_axis": 70, "height": 20, "width":20, "color" : "red" }];
 
 var svgContainer = d3.select("#xnt-viz").append("svg")
