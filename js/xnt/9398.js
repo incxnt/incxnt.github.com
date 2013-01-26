a70e2fd798f7de27c26e4cfbbc456a0598cb364e
Xinyu <claliu@gmail.com>
2013-01-26 16:02:21 +0800

    china

--- js/xnt/9398.js
+++ js/xnt/9398.js
@@ -17,11 +17,16 @@ d3.json("china.json", function(error, china) {
     
     var chinaProvince = topojson.object(china, china.objects.chinaProvince);
     
+    svg.append("path")
+        .datum(chinaProvince)
+        .attr("d", path);
+    
+    /*
     svg.selectAll("chinaProvince")
         .data(chinaProvince)
         .enter()
         .append("path")
         .attr("class", function(d) {return "chinaProvince " + d.id;})
         .attr("d", path);
-    
+    */
 });
