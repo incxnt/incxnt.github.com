e1c21624123a4e4c68a83df96b81768766a6f3ee
Xinyu <claliu@gmail.com>
2013-01-26 14:36:19 +0800

    china

--- js/xnt/9398.js
+++ js/xnt/9398.js
@@ -1,20 +1,20 @@
 
 var width = 1000,
     height = 800;
-/*
+
 var projection = d3.geo.mercator()
-    .scale(6000)
+    .scale(500)
     .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
     .projection(projection);
-*/
+
 var svg = d3.select("#xnt-box").append("svg")
     .attr("width", width)
     .attr("height", height);
 
 d3.json("china.json", function(error, china) {
-  svg.append("path")
-      .datum(topojson.object(china, china.objects.chinaProvince))
-      .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+    svg.append("path")
+        .datum(topojson.object(china, china.objects.chinaProvince))
+        .attr("d", path);
 });
